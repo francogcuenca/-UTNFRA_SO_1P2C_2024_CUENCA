1515a9d4a3a00d8db1a713c0a8a0599c437f7519
francogcuenca <francog.cuenca@gmail.com>
2024-10-03 20:08:12 +0000

    Add desarrollo script B

--- RTA_SCRIPT_Examen_20241002/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_B.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-DISCO10G=$(lsblk -l | grep "10G" | awk '{print $1}')
-echo $DISCO10G
+DISCO=$(lsblk -l | grep "10G" | awk '{print $1}')
+DISCOPATH=$(echo '/dev/'$DISCO)
+echo $DISCOPATH
+sudo fdisk $DISCOPATH << EOF
+n
+e
+
+
+
+w
+EOF
+
+for i in {1..10}; do
+	echo 'Realizando la particion numero '$i
+	sudo fdisk $DISCOPATH << EOF
+n
+l
+$i
+
++1G
+EOF
+done
+sudo fdisk $DISCOPATH << EOF
+w
+EOF
+sudo partprobe $DISCOPATH
