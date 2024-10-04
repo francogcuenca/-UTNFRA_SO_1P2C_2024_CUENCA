4f1ea300883a03e8659130f762c53bbb6bbfbcb9
francogcuenca <francog.cuenca@gmail.com>
2024-10-04 19:59:22 +0000

    Add principio punto C

---
+++ RTA_SCRIPT_Examen_20241002/Punto_C.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MIPASSWORD=$(sudo grep "$(whoami):" /etc/shadow | awk -F: {print })
+
+sudo groupadd p1c2_2024_gAlumno
+sudo groupadd p1c2_2024_gProfesores
+for i in 1 2 3; do
+sudo useradd -g p1c2_2024_gAlumno p1c2_2024_A$i
+echo "p1c2_2024_A$i:$MIPASSWORD" | sudo chpasswd
+done
+sudo useradd -g p1c2_2024_gProfesores p1c2_2024_P1
