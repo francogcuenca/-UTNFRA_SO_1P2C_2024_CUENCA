fb1d35b7724e6e75afbf24b808c894a5320d2ce1
francogcuenca <francog.cuenca@gmail.com>
2024-10-04 12:23:22 +0000

    Update finalizado script B

--- RTA_SCRIPT_Examen_20241002/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_B.sh
@@ -20,13 +20,27 @@ n
  w
 EOF
 done
-sudo partprobe $DISCOPATH
+sudo partprobe
 sudo lsblk $DISCOPATH
 LISTAPARTICIONES=$(sudo fdisk -l $DISCOPATH | awk 'NR >=11 {print $1}')
-$PUNTOSMONTEJE << EOF 
-/Examenes-UTN/
-EOF
-for i in $LISTAPARTICIONES; do
-	sudo mkfs.ext4 $i
-
+particiones=($LISTAPARTICIONES)
+puntos_montaje=( 
+'/Examenes-UTN/alumno_1/parcial_1'
+'/Examenes-UTN/alumno_1/parcial_2'
+'/Examenes-UTN/alumno_1/parcial_3'
+'/Examenes-UTN/alumno_2/parcial_1'
+'/Examenes-UTN/alumno_2/parcial_2'
+'/Examenes-UTN/alumno_2/parcial_3'
+'/Examenes-UTN/alumno_3/parcial_1'
+'/Examenes-UTN/alumno_3/parcial_2'
+'/Examenes-UTN/alumno_3/parcial_3'
+'/Examenes-UTN/profesores'
+)
+for i in "${!particiones[@]}"; do
+	echo "formateando particion $i"
+	sudo mkfs.ext4 -F ${particiones[$i]}
+	echo "Montando particion $i"
+	sudo mount ${particiones[$i]} ${puntos_montaje[$i]}
+	UUID=$(sudo blkid -s UUID -o value ${particiones[$i]})
+	echo 'UUID=$UUID ${puntos_montaje[$i] ext4 defaults 0 2' | sudo tee -a /etc/fstab
 done
